8824ce4ed53632bbbb566943b39c51b43eef8306
anonymity <1176989591@qq.com>
2022-05-24 21:42:45 +0800

    feat: for循环

--- base/for/for.go
+++ base/for/for.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	print9x()
+	printString()
+	printString2()
 }
 
 func print9x() {
@@ -12,5 +14,20 @@ func print9x() {
 			fmt.Printf("%dx%d=%d ", i, j, i*j)
 		}
 		fmt.Println()
+	}
+}
+
+func printString() {
+	var str string = "hello world"
+	for i := 0; i < len(str); i++ {
+		fmt.Println()
+		fmt.Printf("%d, %s", str[i], string(str[i]))
+	}
+}
+
+func printString2() {
+	var str string = "hello world"
+	for i, v := range str {
+		fmt.Printf("str[%d], %c\n", i, v)
 	}
 }
